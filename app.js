d1dd6b1d107f733b293159ba493a405f6de1ef55
lithe07 <99078068@mydavinci.nl>
2026-01-25 13:51:08 +0100

    add assets

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("portfolio geladen");
+function toggleMenu() {
+    const menu = document.getElementById("mobileMenu");
+    const btn = document.getElementById("menuButton");
+  
+    menu.classList.toggle("hidden");
+  
+    const isOpen = !menu.classList.contains("hidden");
+    btn.setAttribute("aria-expanded", String(isOpen));
+  }
